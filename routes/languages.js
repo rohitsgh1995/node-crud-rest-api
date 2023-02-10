b93e1b43285d9cb6fb56c8c930f4253f5ad5371c
rohitsgh1995 <rohit1995sgh@gmail.com>
2023-02-10 17:43:10 +0530

    create and update query and route

--- routes/languages.js
+++ routes/languages.js
@@ -12,4 +12,22 @@ router.get('/', async function (req, res, next) {
     }
 });
 
+router.post('/', async function(req, res, next) {
+    try {
+        res.json(await languages.create(req.body));
+    } catch (err) {
+        console.error(`Error while creating new language.`, err.message);
+        next(err);
+    }
+});
+
+router.put('/:id', async function(req, res, next) {
+    try {
+        res.json(await languages.update(req.params.id, req.body));
+    } catch (err) {
+        console.error(`Error while updating language.`, err.message);
+        next(err);
+    }
+});
+
 module.exports = router;
--- services/languages.js
+++ services/languages.js
@@ -16,6 +16,38 @@ async function getMultiple(page = 1) {
     }
 }
 
+async function create(language) {
+    const result = await db.query(
+        `INSERT INTO programming_languages (name, released_year, githut_rank, pypl_rank, tiobe_rank) VALUES
+        (${language.name}, ${language.released_year}, ${language.githut_rank}, ${language.pypl_rank}, ${language.tiobe_rank})`
+    );
+
+    let message = 'Error in creating new langauge.';
+
+    if (result.affectedRows) {
+        message = 'New language created successfully.';
+    }
+
+    return {message};
+}
+
+async function update(id, language) {
+    const result = await db.query(
+        `UPDATE programming_languages SET name="${language.name}", released_year=${language.released_year}, githut_rank=${language.githut_rank},
+        pypl_rank=${language.pypl_rank}, tiobe_rank=${language.tiobe_rank}) WHERE id=${id}`
+    );
+
+    let message = 'Error in updating langauge.';
+
+    if (result.affectedRows) {
+        message = 'Language updated successfully.';
+    }
+
+    return {message};
+}
+
 module.exports = {
-    getMultiple
+    getMultiple,
+    create,
+    update
 }
